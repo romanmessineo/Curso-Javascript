edcde2f0787c6973e1a504256cf0d75bef794066
roman.messieo <messineoroman@gmail.com>
2022-07-31 18:40:48 -0300

    [se carga modo de local storange]

--- javascript/darkmode.class.js
+++ javascript/darkmode.class.js
@@ -1,32 +1,17 @@
-
 const colorSwitch = document.querySelector('#switch input[type="checkbox"]');
 
-
 function cambiaTema(ev) {
-    if (ev.target.checked) {
-        document.documentElement.setAttribute('tema', 'light');
-
-    } else {
-        document.documentElement.setAttribute('tema', 'dark');
-    }
-    localStorage.setItem("modo", (ev.target.checked));
+  if (ev.target.checked) {
+    document.documentElement.setAttribute("tema", "light");
+  } else {
+    document.documentElement.setAttribute("tema", "dark");
+  }
+  localStorage.setItem("modo", ev.target.checked);
 }
-colorSwitch.addEventListener('change', cambiaTema);
+colorSwitch.addEventListener("change", cambiaTema);
 
-
-//No pue hacer que la paginame carge las preferencia desde el localstorange
-
-
-function cargarPreferencias() {
-    const mode = localStorage.getItem("modo")
-
-    if (mode) //mode !=== null, undefined, 0, false, ""
-    {
-        document.documentElement.setAttribute(`tema`, cambiaTema(mode))
-
-        /* console.log(cambiaTema(mode)) */
-    }
+if (localStorage.getItem(`modo`) === `true`) {
+  document.documentElement.setAttribute("tema", "light");
+} else {
+  document.documentElement.setAttribute("tema", "dark");
 }
-
-
-
